785168aca92f7bba1a948660ffa2d1fcd747e1f7
jelly-go <63229189+jelly-go@users.noreply.github.com>
2020-04-06 16:32:09 +0800

    Update vpstest.sh

--- vpstest.sh
+++ vpstest.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-blue(){
-    echo -e "\033[34m\033[01m$1\033[0m"
-}
-green(){
-    echo -e "\033[32m\033[01m$1\033[0m"
-}
-yellow(){
-    echo -e "\033[33m\033[01m$1\033[0m"
-}
-red(){
-    echo -e "\033[31m\033[01m$1\033[0m"
-}
 
 if [[ -f /etc/redhat-release ]]; then
     release="centos"
